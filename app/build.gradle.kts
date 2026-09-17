2013c946bde9dd98fe9b98ffae2b06de3d2aff77
sizwe3066-cpu <sizwe3066@gmail.com>
2026-09-17 18:24:25 +0200

    Create PantryActivity and RecycleView

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,6 +32,7 @@ android {
 }
 
 dependencies {
+    implementation("androidx.recyclerview:recyclerview:1.3.2")
     implementation(libs.activity.ktx)
     implementation(libs.appcompat)
     implementation(libs.constraintlayout)
