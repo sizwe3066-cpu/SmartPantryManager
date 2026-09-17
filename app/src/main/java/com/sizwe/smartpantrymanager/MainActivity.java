16bef388ffac0cd653012328e5a0222950fa1de7
sizwe3066-cpu <sizwe3066@gmail.com>
2026-09-17 18:52:09 +0200

    Navigate from MainActivity to PantryActivity

--- app/src/main/java/com/sizwe/smartpantrymanager/MainActivity.java
+++ app/src/main/java/com/sizwe/smartpantrymanager/MainActivity.java
@@ -1,13 +1,11 @@
 package com.sizwe.smartpantrymanager;
-import com.sizwe.smartpantrymanager.database.DatabaseHelper;
 
+import android.content.Intent;
 import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import com.sizwe.smartpantrymanager.database.DatabaseHelper;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,17 +13,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        EdgeToEdge.enable(this);
-
-        setContentView(R.layout.activity_main);
-
         DatabaseHelper dbHelper = new DatabaseHelper(this);
         dbHelper.getWritableDatabase();
 
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        Intent intent = new Intent(this, PantryActivity.class);
+        startActivity(intent);
+        finish();
+
     }
 }
